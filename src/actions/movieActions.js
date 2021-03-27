580335b693c804dbfc0b62619d6e28ca0c998e77
Tenrog <54127235+Tenrog@users.noreply.github.com>
2021-03-27 15:01:03 -0600

    Trying to show movie list

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -31,7 +31,7 @@ export function setMovie(movie) {
 export function fetchMovies() {
     const env = runtimeEnv();
     return dispatch => {
-        return fetch(`${env.REACT_APP_API_URL}/movies?reviews=true`, {
+        return fetch(`${env.REACT_APP_API_URL}/movies?`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -43,6 +43,7 @@ export function fetchMovies() {
                 if (!response.ok) {
                     throw Error(response.statusText);
                 }
+                console.log(response.json());
                 return response.json();
             })
             .then( (res) => {
