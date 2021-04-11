000112ab83b9d5916df50e4bd0430ddd73db7a15
Tenrog <54127235+Tenrog@users.noreply.github.com>
2021-04-11 14:14:38 -0600

    HW5 commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import MovieHeader from './components/movieheader';
-import MovieList from './components/movielist';
-import Movie from './components/movie';
+import MovieHeader from './components/Defunct/movieheader';
+import MovieList from './components/Defunct/movielist';
+import Movie from './components/Defunct/movie';
 import Authentication from './components/authentication';
 import {HashRouter,Route} from 'react-router-dom';
 import { Provider } from 'react-redux'
--- src/components/Defunct/movielist.js
+++ src/components/Defunct/movielist.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { fetchMovies } from "../actions/movieActions";
-import { setMovie } from "../actions/movieActions";
-import {connect} from 'react-redux';
-import {Image, Nav} from 'react-bootstrap';
-import { Carousel } from 'react-bootstrap';
-import { BsStarFill} from 'react-icons/bs'
+import { fetchMovies } from '../../actions/movieActions';
+import { setMovie } from '../../actions/movieActions';
+import {connect} from "react-redux";
+import { Image } from 'react-bootstrap'
+import { Carousel } from 'react-bootstrap'
+import { Glyphicon } from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap';
 
 class MovieList extends Component {
@@ -29,34 +29,31 @@ class MovieList extends Component {
     }
 
     render() {
-        const MovieListCarousel = ({movieList}) => {
-            if (!movieList) {
-                return <div>Loading....</div>
+        const MovieListCarousel= ({movieList}) => {
+            if (!movieList) { // evaluates to true if currentMovie is null
+                return <div>Loading...</div>;
             }
 
             return (
                 <Carousel onSelect={this.handleSelect}>
                     {movieList.map((movie) =>
-                        <Carousel.Item key={movie.title}>
+                        <Carousel.Item key={movie._id}>
                             <div>
-                                <LinkContainer to={'/movie/'+movie.title} onClick={()=>this.handleClick(movie)}>
-                                    <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
+                                <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
+                                    <Image className="image" src={movie.imageUrl} thumbnail />
                                 </LinkContainer>
                             </div>
                             <Carousel.Caption>
                                 <h3>{movie.title}</h3>
-                                <BsStarFill glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+                                <Glyphicon glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
                             </Carousel.Caption>
-                        </Carousel.Item>
-                    )}
-
-                </Carousel>
-            )
+                        </Carousel.Item>)}
+                </Carousel>)
         }
 
         return (
-            <MovieListCarousel movieList={this.props.movies} />
-        )
+          <MovieListCarousel movieList={this.props.movies} />
+        );
     }
 }
 
